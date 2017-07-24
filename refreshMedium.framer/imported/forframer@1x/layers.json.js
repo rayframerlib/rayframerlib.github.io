2d48ad2ac46fdfac4dd481fefbadfebb4a097f72
blackray12 <blackray.12@gmail.com>
2017-07-24 18:59:18 +0800

    add

---
+++ refreshMedium.framer/imported/forframer@1x/layers.json.js
@@ -0,0 +1,282 @@
+window.__imported__ = window.__imported__ || {};
+window.__imported__["forframer@1x/layers.json.js"] = [
+	{
+		"objectId": "374FD111-5E0B-450F-AB38-B9C5E359EDD5",
+		"kind": "artboard",
+		"name": "mainScreen",
+		"originalName": "mainScreen",
+		"maskFrame": null,
+		"layerFrame": {
+			"x": 166,
+			"y": 184,
+			"width": 750,
+			"height": 1334
+		},
+		"visible": true,
+		"metadata": {},
+		"backgroundColor": "rgba(255, 255, 255, 1)",
+		"children": [
+			{
+				"objectId": "E5CDFEC9-34F5-4C80-AF32-897DC30B4BF7",
+				"kind": "group",
+				"name": "navi",
+				"originalName": "navi",
+				"maskFrame": null,
+				"layerFrame": {
+					"x": 0,
+					"y": 0,
+					"width": 750,
+					"height": 131
+				},
+				"visible": true,
+				"metadata": {
+					"opacity": 1
+				},
+				"image": {
+					"path": "images/Layer-navi-rtvdrezf.png",
+					"frame": {
+						"x": 0,
+						"y": 0,
+						"width": 750,
+						"height": 131
+					}
+				},
+				"children": []
+			},
+			{
+				"objectId": "05EF230B-158F-408A-88BA-F971F8619236",
+				"kind": "group",
+				"name": "tab",
+				"originalName": "tab",
+				"maskFrame": null,
+				"layerFrame": {
+					"x": 0,
+					"y": 1239,
+					"width": 750,
+					"height": 95
+				},
+				"visible": true,
+				"metadata": {
+					"opacity": 1
+				},
+				"children": [
+					{
+						"objectId": "397D0F77-5021-439D-88AE-16CC0AB9CA34",
+						"kind": "group",
+						"name": "homeTab",
+						"originalName": "homeTab",
+						"maskFrame": null,
+						"layerFrame": {
+							"x": 36,
+							"y": 1239,
+							"width": 80,
+							"height": 89
+						},
+						"visible": true,
+						"metadata": {
+							"opacity": 1
+						},
+						"children": [
+							{
+								"objectId": "E7430DA7-F009-4E1A-BFE2-903B6956FB0E",
+								"kind": "group",
+								"name": "notice",
+								"originalName": "notice",
+								"maskFrame": null,
+								"layerFrame": {
+									"x": 86,
+									"y": 1256,
+									"width": 30,
+									"height": 30
+								},
+								"visible": true,
+								"metadata": {
+									"opacity": 1
+								},
+								"image": {
+									"path": "images/Layer-notice-rtc0mzbe.png",
+									"frame": {
+										"x": 86,
+										"y": 1256,
+										"width": 30,
+										"height": 30
+									}
+								},
+								"children": []
+							},
+							{
+								"objectId": "70862E21-313C-4006-BD27-CEDCC4D8BAB7",
+								"kind": "group",
+								"name": "jsonArea",
+								"originalName": "jsonArea",
+								"maskFrame": null,
+								"layerFrame": {
+									"x": 36,
+									"y": 1239,
+									"width": 80,
+									"height": 80
+								},
+								"visible": true,
+								"metadata": {
+									"opacity": 1
+								},
+								"image": {
+									"path": "images/Layer-jsonArea-nza4njjf.png",
+									"frame": {
+										"x": 36,
+										"y": 1239,
+										"width": 80,
+										"height": 80
+									}
+								},
+								"children": []
+							},
+							{
+								"objectId": "BF8AB161-3029-4DB8-999B-D1EA185F1029",
+								"kind": "group",
+								"name": "icon",
+								"originalName": "icon",
+								"maskFrame": null,
+								"layerFrame": {
+									"x": 54,
+									"y": 1258,
+									"width": 44,
+									"height": 44
+								},
+								"visible": true,
+								"metadata": {
+									"opacity": 1
+								},
+								"image": {
+									"path": "images/Layer-icon-qky4quix.png",
+									"frame": {
+										"x": 54,
+										"y": 1258,
+										"width": 44,
+										"height": 44
+									}
+								},
+								"children": []
+							},
+							{
+								"objectId": "E934601A-517C-4F32-8CD6-B487FA4BB188",
+								"kind": "text",
+								"name": "text",
+								"originalName": "text",
+								"maskFrame": null,
+								"layerFrame": {
+									"x": 56,
+									"y": 1308,
+									"width": 39,
+									"height": 20
+								},
+								"visible": true,
+								"metadata": {
+									"opacity": 1,
+									"string": "首页",
+									"css": [
+										"/* 首页: */",
+										"font-family: .PingFangSC-Regular;",
+										"font-size: 20px;",
+										"color: #5D5D5D;"
+									]
+								},
+								"image": {
+									"path": "images/Layer-text-rtkzndyw.png",
+									"frame": {
+										"x": 56,
+										"y": 1308,
+										"width": 39,
+										"height": 20
+									}
+								},
+								"children": []
+							}
+						]
+					},
+					{
+						"objectId": "6E0FA846-37E9-4CD2-A31C-E5BF7E504B48",
+						"kind": "group",
+						"name": "tabBg",
+						"originalName": "tabBg",
+						"maskFrame": null,
+						"layerFrame": {
+							"x": 0,
+							"y": 1245,
+							"width": 750,
+							"height": 89
+						},
+						"visible": true,
+						"metadata": {
+							"opacity": 1
+						},
+						"image": {
+							"path": "images/Layer-tabBg-nkuwrke4.png",
+							"frame": {
+								"x": 0,
+								"y": 1245,
+								"width": 750,
+								"height": 89
+							}
+						},
+						"children": []
+					}
+				]
+			},
+			{
+				"objectId": "11FEAD42-0A51-409F-87FF-8C355BC4AA0A",
+				"kind": "group",
+				"name": "feed",
+				"originalName": "feed",
+				"maskFrame": null,
+				"layerFrame": {
+					"x": 0,
+					"y": 151,
+					"width": 750,
+					"height": 3000
+				},
+				"visible": true,
+				"metadata": {
+					"opacity": 1
+				},
+				"image": {
+					"path": "images/Layer-feed-mtfgrufe.png",
+					"frame": {
+						"x": 0,
+						"y": 151,
+						"width": 750,
+						"height": 3000
+					}
+				},
+				"children": []
+			},
+			{
+				"objectId": "D9142E71-D696-4B04-AB95-0DAED5EE59CE",
+				"kind": "group",
+				"name": "bg",
+				"originalName": "bg",
+				"maskFrame": null,
+				"layerFrame": {
+					"x": 0,
+					"y": 0,
+					"width": 750,
+					"height": 1334
+				},
+				"visible": true,
+				"metadata": {
+					"opacity": 1
+				},
+				"image": {
+					"path": "images/Layer-bg-rdkxndjf.png",
+					"frame": {
+						"x": 0,
+						"y": 0,
+						"width": 750,
+						"height": 1334
+					}
+				},
+				"children": []
+			}
+		]
+	}
+]
